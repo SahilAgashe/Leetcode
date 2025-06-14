83433197e51a63217144cf0a3add3752726c392a
Sahil Agashe <sahilagashe2002@gmail.com>
2025-06-14 13:15:41 +0530

    Updated 242.swift

--- 242.swift
+++ 242.swift
@@ -5,6 +5,7 @@
 class Solution {
     func isAnagram(_ s: String, _ t: String) -> Bool {
         optimalSolve(s,t)
+        //optimalWithStringIndex(s,t)
         //naiveSolve(s,t)
     }
 
@@ -26,6 +27,33 @@ class Solution {
             if let value = charMap[t[i]] {
                 charMap[t[i]] = value - 1
             } else { charMap[t[i]] = -1 }
+        }
+
+        for value in charMap.values {
+            if value != 0 { return false }
+        }
+        return true
+    }
+
+    // Giving TLE , As Used Strig Index 
+    // Accessing String.Index takes O(n) TC
+    // So It Will O(n^2) TC
+    func optimalWithStringIndex(_ s: String, _ t: String) -> Bool {
+        if s.count != t.count { return false }
+
+        var charMap = Dictionary<Character,Int>() // Character <--> Frequency
+        let n = s.count
+
+        for i in 0..<n {
+            let sIndex = s.index(s.startIndex, offsetBy: i)
+            if let value = charMap[s[sIndex]] {
+                charMap[s[sIndex]] = value + 1
+            } else { charMap[s[sIndex]] = 1 }
+
+            let tIndex = t.index(t.startIndex, offsetBy: i)
+            if let value = charMap[t[tIndex]] {
+                charMap[t[tIndex]] = value - 1
+            } else { charMap[t[tIndex]] = -1 }
         }
 
         for value in charMap.values {
