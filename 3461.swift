ecdc4d5591b1fcc8df6ab0db4c8a40fb1b821852
Sahil Amrut Agashe <90738086+SahilAgashe@users.noreply.github.com>
2026-03-16 19:52:10 +0530

    Updated solve for 3461

--- 3461.swift
+++ 3461.swift
@@ -3,62 +3,24 @@
 // Easy 
 
 class Solution {
-    // Better Naive Solution 
-    func hasSameDigits(_ s: String) -> Bool {
-        let n = s.count
-        var digits = s.map { Int(String($0)) ?? 0 }
-
-        if n == 2 {
-            return digits[0] == digits[1] ? true : false
-        }
-
-        // for n > 2 , 
-        // there will be n - 2 operations
-        var counter = n - 2 
-        var updatedSize = n
-        while counter > 0 {
-            for i in 1..<updatedSize {
-                digits[i - 1] = (digits[i] + digits[i - 1]) % 10
-            }
-            counter -= 1
-            updatedSize -= 1
-        }
-        return digits[0] == digits[1] ? true : false
-    }
-
-}
-
-class NaiveSolution {
-    func hasSameDigits(_ s: String) -> Bool {
-        var n = s.count
-        var s = s
-        var newString: String = ""
-        while n > 2 {
-            newString = operation(s)
-            s = newString
-            n = newString.count
-        }
-
-        if newString.count == 2 {
-            let array = Array<Character>(newString)
-            return array.first == array[1] ? true : false
-        }
-
-        return false
-    }
-
-    func operation(_ s: String) -> String {
-        let s = Array<Character>(s)
+    func hasSameDigits(_ s: String) -> Bool { 
         let n = s.count 
-        if n <= 2 { return ""}
-        var result = ""
-        for i in 1..<n {
-            let strCh: String = String(s[i])
-            let digit = Int(strCh) ?? 0
-            let prevDigit = Int(String(s[i - 1])) ?? 0
-            let sum = (digit + prevDigit) % 10 
-            result.append("\(sum)")
+        var digits: [Int] = s.map { Int(String($0)) ?? 0 }
+
+        // base conditions
+        if n < 2 { return false}
+        if n == 2 { return digits[0] == digits[1] }
+
+        // if n > 2 
+        var temp = n
+        var lastIndex = n - 1
+        while lastIndex >= 2 {
+            for i in 0...(lastIndex - 1) {
+                let t = digits[i] + digits[i + 1]
+                digits[i] = t % 10
+            }
+            lastIndex -= 1
         }
-        return result
+        return digits[0] == digits[1]
     }
 }
