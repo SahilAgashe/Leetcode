2d45dc7d504b99a8fbdd6831fa4832ee37d04a3f
Sahil Amrut Agashe <90738086+SahilAgashe@users.noreply.github.com>
2026-07-28 00:04:17 +0530

    Solved Leetcode 75.Sort Colors
    DNF Algorithm (Dutch National Flag Algorithm) OR 3-way partitioning Algorithm

---
+++ 75.swift
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/sort-colors/description/
+//Leetcode 75.Sort Colors
+// Medium (As per SAA, It is very once you understand!!! And You can Believe That!)
+
+class Solution {
+    // DNF Algorithm (Dutch National Flag Algorithm) OR 3-way partitioning Algorithm
+    func sortColors(_ nums: inout [Int]) {
+        let n = nums.count
+        var low = 0
+        var mid = 0 
+        var high = n - 1
+
+        while mid <= high {
+            if nums[mid] == 0 {
+                nums.swapAt(mid, low)
+                low += 1
+                mid += 1
+            } else if nums[mid] == 1 {
+                mid += 1
+            } else { // else if nums[mid] == 2
+                nums.swapAt(mid, high)
+                high -= 1
+            }
+        }
+    }
+}
+
+// 0 to low - 1 (All zeros)
+// low to mid - 1 (All ones)
+// mid to high (Unexplored area) (xxxxx)
+// high + 1 to n - 1 (All twos)
+
+// Imagine array as using above indexing
+// 00000 11111 xxxxx 22222
+
+// Solution follow => Striver's Video(Better) or Gemini Explaination for understanding!. It's very easy!!!
