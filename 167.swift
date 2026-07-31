537e6abad02842cb3c003c7f7b9f56f6bb70dcbd
Sahil Amrut Agashe <90738086+SahilAgashe@users.noreply.github.com>
2026-07-31 08:14:24 +0530

    Update 167.swift

--- 167.swift
+++ 167.swift
@@ -7,12 +7,18 @@ class Solution {
         let n = numbers.count
         var low = 0
         var high = n - 1
-        while low < high {
+        var ans = [Int]()
+        while (low < high) {
             let sum = numbers[low] + numbers[high]
-            if sum == target { return [low + 1, high + 1]}
-            else if sum > target { high = high - 1}
-            else { low = low + 1}
+            if sum == target {
+                ans = [low + 1, high + 1]
+                break;
+            } else if sum < target { 
+                low += 1 
+            } else { // sum > target
+                high -= 1
+            }
         }
-        return []
-    }
+        return ans
+    }   
 }
